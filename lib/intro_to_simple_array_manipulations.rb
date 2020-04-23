fb745d869dd6286daddcb810cd2f92055e078be6
Michael Jones <djyoungjones@gmail.com>
2020-04-23 15:23:10 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,5 @@
+def  using_concat(my_fav, more_favs)
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  my_favorite_things.concat("sports cars", "flatiron school")
+end
